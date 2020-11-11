45c8940478a33b2587e6333753697958cfe7422a
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2020-11-11 19:41:46 +0100

    add nodemon, fix async in hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,24 @@
 import { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import axios from 'axios';
 
-const App = (props) => {
+const App = () => {
   const [serverMessage, setServerMessage] = useState('');
-  useEffect(async () => {
-    try{
-      const resp = await axios.get("http://localhost:5000")
-      setServerMessage(resp.data);
-    } catch(e) {
-      setServerMessage("error from server");
+  useEffect(() => {
+    const fetchHello = async () => {
+      try {
+        const resp = await axios.get("http://localhost:5000")
+        setServerMessage(resp.data);
+      } catch(e) {
+        setServerMessage("error from server");
+      }
     }
-  }, [props]);
+    fetchHello()
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        server message: {serverMessage}
-      </div>
+    <div>
+      server message: {serverMessage}
     </div>
   );
 }
